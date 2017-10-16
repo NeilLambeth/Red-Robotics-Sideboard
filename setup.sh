c4feacbeae61871dd74dde70c07b8624a5a02ae1
Neil <neillambeth@gmail.com>
2017-10-17 00:28:08 +0100

    add setup.sh

---
+++ setup.sh
@@ -0,0 +1,43 @@
+#Red Robotics set up script
+
+#Neopixel set up taken from the original Adafruit tutorial: 
+#https://learn.adafruit.com/neopixels-on-raspberry-pi/software  
+
+
+
+
+cd 
+sudo apt-get update && sudo apt-get upgrade -y
+printf "Upgrade"
+
+sudo apt-get install python3-dev python-dev python-pip python3-pip joystick -y
+sudo pip3 install evdev
+
+sudo apt-get install build-essential git scons swig -y
+
+git clone https://github.com/NeilLambeth/Red-Robotics-Sideboard.git
+cd Red-Robotics-Sideboard
+cp * /home/pi
+
+cd
+git clone https://github.com/jgarff/rpi_ws281x.git
+cd rpi_ws281x
+scons
+
+cd python
+sudo python setup.py install
+
+cd
+rm pigpio.zip
+sudo rm -rf PIGPIO
+wget abyz.co.uk/rpi/pigpio/pigpio.zip
+unzip pigpio.zip
+cd PIGPIO
+make
+sudo make install
+
+cd
+sudo sed -i -e '$i #start Pgpio deamon\nsudo pigpiod\n' /etc/rc.local
+
+sudo sed -i -e '$i #start reset_shutdown script\nsudo python /home/pi/reset_shutdown.py&' /etc/rc.local
+
