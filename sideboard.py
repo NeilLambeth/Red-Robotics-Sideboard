942a54918d6c95f76087c70d470794f4fcb5732b
Neil <neillambeth@gmail.com>
2017-10-19 22:29:39 +0100

    max speed 100 fix

--- sideboard.py
+++ sideboard.py
@@ -35,34 +35,60 @@ print("Sideboard loaded")
 
 
         
-def r_motor(rMotor):   
+def r_motor(rm):   
+
+            if rm > 100:  # Make sure the value sent to the motor is 100 or less
+                print("Out of range")
+                rm = 100
+
+            elif rm < -100:  # Make sure the value sent to the motor is 100 or less
+                print("Out of range")
+                rm = -100
+
+            rMotor = rm * 2.55
 
             # Set right motor direction
             if rMotor > 0:  
                 pi.write(dira, FWD)  # Go forwards
                 RM = rMotor
-                print ("Right Motor = "),(RM)
+                print("Right Motor ="),(rm)
+                #print("Actual = "),(RM)
+
             elif rMotor < 0:  
                 pi.write(dira, BWD)  # Go backwards
                 RM = abs(rMotor)  # Make positive
-                print("Right Motor = -"),(RM)
+                print("Right Motor ="),(rm)
+                #print("Actual = -"),(RM)
             else:
                 print("Right Stop")
-                RM = 0  # Stop
-            
+                RM = 0  # Stop            
+ 
             pi.set_PWM_dutycycle(pwma,RM)
 
 
-def l_motor(lMotor):                
+def l_motor(lm):  
+
+            if lm > 100:  # Make sure the value sent to the motor is 100 or less
+                print("Out of range")
+                lm = 100
+
+            elif lm < -100:  # Make sure the value sent to the motor is 100 or less
+                print("Out of range")
+                lm = -100
+
+            lMotor = lm * 2.55
+              
             # Set left motor direction
             if lMotor > 0:
                 pi.write(dirb, FWD)  # Go forwards
                 LM = lMotor
-                print("Left Motor  = "),(LM)
+                print("Left Motor  ="),(lm)
+                #print("Actual = "),(LM)
             elif lMotor < 0:  
                 pi.write(dirb, BWD)  # Go backwards
                 LM = abs(lMotor)  # Make positive
-                print("Left Motor  = -"),(LM)
+                print("Left Motor  ="),(lm)
+                #print("Actual = -"),(LM)
             else:
                 print("Left Stop")
                 LM = 0  # Stop  
